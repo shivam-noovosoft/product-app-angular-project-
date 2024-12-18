2e3159b422e759c9324afe9f1ad6d5c78a61c0b2
shivam-chouksey <shivam.chouksey@noovosoft.com>
2024-12-18 18:42:40 +0530

    Fix: all user's cart visibility in admin page

--- Product-App/src/app/services/loggedUser.service.ts
+++
@@ -1,8 +0,0 @@
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
-import {User} from '../models/users.models';
-
-@Injectable({providedIn: 'root'})
-class LoggedUserService {
-  loggedUserData = new Subject<User>()
-}
